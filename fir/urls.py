7ff0babd887cadd8e92391d1db03e090e4bf9643
Nitesh Nagpal <niteshnagpal@outlook.com>
2021-10-17 12:56:15 +0530

    requirements and readme file added

--- fir/urls.py
+++ fir/urls.py
@@ -1,4 +1,4 @@
-"""firmingle URL Configuration
+"""fir App URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -13,10 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # admin route for App
-    path('', include('fir.urls')),  # inlcude App urls
+    path('', views.home, name='index'),
 ]
