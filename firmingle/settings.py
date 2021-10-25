ab4c2fe156fb67436b6b35d62049fc93f3b7bdc9
Nitesh Nagpal <niteshnagpal@outlook.com>
2021-10-25 23:08:27 +0530

    deploy

--- firmingle/settings.py
+++ firmingle/settings.py
@@ -30,8 +30,7 @@ DEBUG = False
 
 APPEND_SLASH = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -46,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,11 +129,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
